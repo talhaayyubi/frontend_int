01eee1cc0be53fbd6469c0a195356738c2507d10
Talha Salahuddin <59451858+talhaayyubi@users.noreply.github.com>
2025-09-08 05:46:08 +0500

    Revert "feat: establish design tokens and hero"

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "../components/organisms/Hero";
-
-export default function Home() {
-  return (
-    <main className="font-sans flex items-center justify-center min-h-screen p-8 sm:p-20">
-      <Hero />
-    </main>
-  );
-}
