990e3b94f0834932289aa7968e8c8617526a710e
Talha Salahuddin <59451858+talhaayyubi@users.noreply.github.com>
2025-09-08 05:59:05 +0500

    feat: add GitHub-style repo interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "../components/organisms/Hero";
+import GithubRepo from "../components/organisms/GithubRepo";
 
 export default function Home() {
   return (
-    <main className="font-sans flex items-center justify-center min-h-screen p-8 sm:p-20">
-      <Hero />
+    <main className="font-sans">
+      <GithubRepo />
     </main>
   );
 }
